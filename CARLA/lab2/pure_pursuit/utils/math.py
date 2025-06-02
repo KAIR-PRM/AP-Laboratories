240ab29e9476e4a0a39374a65ac1409bac1cd113
Marek Długosz <81410184+mdlugosz-agh@users.noreply.github.com>
2025-06-02 15:20:20 +0200

    Update math.py

--- CARLA/lab2/pure_pursuit/utils/math.py
+++ CARLA/lab2/pure_pursuit/utils/math.py
@@ -4,4 +4,4 @@ import numpy as np
 
 # TODO: zaimplementuj methodę, by zwracała wektor [x, y, z]
 def carlaVector3DToNumpy(vector: carla.Vector3D) -> np.ndarray:
-    return ...
+    return np.array([vector.x, vector.y, vector.z]) 
